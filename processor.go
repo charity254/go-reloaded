31952958985e04ded7ba92b1e497eb22dba31747
charity254 <atienocharity776@gmail.com>
2026-02-15 13:23:11 +0300

    feat: implement multi-word transformations for all functions

--- processor.go
+++ processor.go
@@ -7,7 +7,22 @@ import (
 
 func hex (str  []string) []string {
 	for i := 0; i < len(str); i++ {
-		if str[i] == "(hex)" && i > 0 {
+		if len(str[i]) > 5 && str[i][:5] == "(hex," && str[i][len(str[i])-1] == ')' {
+			num := str[i][5:len(str[i])-1]
+			n, err := strconv.Atoi(strings.TrimSpace(num))
+			if err != nil {
+				continue
+			}
+			for j := 1; j <= n && i-j >= 0; j++ {
+				value, err := strconv.ParseInt(str[i-j], 16, 64)
+				if err != nil {
+					continue
+				}
+				str[i-j]= strconv.Itoa(int(value))
+			}
+			str = append(str[:i], str[i+1:]... )
+				i--
+		}else if str[i] == "(hex)" && i > 0 {
 			value, err := strconv.ParseInt(str[i-1], 16, 64)
 			 if err != nil {
 				continue
@@ -22,7 +37,22 @@ func hex (str  []string) []string {
 
 func bin (str []string) []string {
 	for i := 0; i < len(str); i++ {
-		if str[i] == "(bin)" && i > 0 {
+		if len(str[i]) > 5 && str[i][:5] == "(bin," && str[i][len(str[i])-1] == ')' {
+			num := str[i][5:len(str[i])-1]
+			n, err := strconv.Atoi(strings.TrimSpace(num))
+			if err != nil {
+				continue
+			}
+			for j := 1; j <= n && i-j >= 0; j++ {
+				value, err := strconv.ParseInt(str[i-j], 2, 64)
+				if err != nil {
+					continue
+				}
+				str[i-j] = strconv.Itoa(int(value))
+			}
+			str = append(str[:i], str[i+1:]... )
+			i--
+		} else if str[i] == "(bin)" && i > 0 {
 			value, err := strconv.ParseInt(str[i-1], 2, 64)
 
 			if err != nil {
@@ -38,20 +68,43 @@ func bin (str []string) []string {
 
 func upper (str []string) []string {
 	for i := 0; i < len(str); i++ {
-		if str[i] == "(up)" && i > 0 {
+		if len(str[i]) > 4 && str[i][:4] == "(up," && str[i][len(str[i])-1] == ')' {
+			num := str[i][4:len(str[i])-1]
+			n, err := strconv.Atoi(strings.TrimSpace(num))
+			if err != nil {
+				continue
+			}
+			for j := 1; j <= n && i-j >= 0; j++ {
+				value := strings.ToUpper(str[i-j])
+				str[i-j] = value
+			}
+			str = append(str[:i], str[i+1:]... )
+			i--
+		}else if str[i] == "(up)" && i > 0 {
 			value := strings.ToUpper(str[i-1])
 			str[i-1] = value
 			str = append(str[:i], str[i+1:]... )
 			i--
-		}
-		
+		}	
 	}
 	return str
 }
 
 func lower (str []string) []string {
 	for i := 0; i < len(str); i++ {
-		if str[i] == "(low)" && i > 0 {
+		if len(str[i]) > 5 && str[i][:5] == "(low," && str[i][len(str[i])-1] == ')' {
+			num := str[i][5:len(str[i])-1]
+			n, err := strconv.Atoi(strings.TrimSpace(num))
+			if err != nil {
+				continue
+			}
+			for j := 1; j <= n && i-j >= 0; j++ {
+				value := strings.ToLower(str[i-j])
+				str[i-j] = value
+			}
+			str = append(str[:i], str[i+1:]... )
+			i--
+		}else if str[i] == "(low)" && i > 0 {
 			value := strings.ToLower(str[i-1])
 			str[i-1] = value
 			str = append(str[:i], str[i+1:]... )
@@ -64,7 +117,30 @@ func lower (str []string) []string {
 
 func cap (str []string) []string {
 	for i := 0; i < len(str); i++ {
-		if str[i] == "(cap)" && i > 0 {
+		if len(str[i]) > 5 && str[i][:5] == "(cap," && str[i][len(str[i])-1] == ')' {
+			num := str[i][5:len(str[i])-1]
+			n, err := strconv.Atoi(strings.TrimSpace(num))
+			if err != nil {
+				continue
+			}
+			for j := 1; j <= n && i-j >= 0; j++ {
+				word := str[i-j]
+				if len(word) == 0 {
+					continue
+				}
+				if len(word) == 1 {
+					word = strings.ToUpper(word)
+					str[i-j] = word
+				} else{
+					fLet := strings.ToUpper(string(word[0]))
+					rWord := strings.ToLower(string(word[1:]))
+					word = fLet+rWord
+					str[i-j] = word
+				}
+			}
+			str= append(str[:i], str[i+1:]... )
+			i--
+		}else if str[i] == "(cap)" && i > 0 {
 			word := str[i-1]
 			if len(word) == 0 {
 				continue
@@ -72,11 +148,12 @@ func cap (str []string) []string {
 			if len(word) == 1 {
 				word = strings.ToUpper(word)
 				str[i-1] = word
+			} else{
+				fLet := strings.ToUpper(string(word[0]))
+				rWord := strings.ToLower(string(word[1:]))
+				word = fLet+rWord
+				str[i-1] = word
 			}
-			fLet := strings.ToUpper(string(word[0]))
-			rWord := strings.ToLower(string(word[1:]))
-			word = fLet+rWord
-			str[i-1] = word
 
 			str = append(str[:i], str[i+1:]... )
 			i--
